92c51f2f76112354fef9c82e42c87fac2cb331ac
Hanametro <61606826+Hanametro@users.noreply.github.com>
2020-03-28 18:07:58 +0800

    20200328更新

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Shanghai-Bus-Track
+
+[ver.2020 03 28]
+增加内容：上海实时公交查询Plus，输入线路和方向即可显示该线路所有车辆，详见示例图
+
+
+
+[ver.2020 03 27]
 获取上海实时公交信息
 
 上海实时公交查询.py：输入公交线路、上下行、车站序号即可查询动态
@@ -7,4 +14,4 @@
 黄渡理工实时公交.py：读取busdata.csv的内容，批量查询黄渡理工周边公交线路
 
 原网页：https://shanghaicity.openservice.kankanews.com/public/bus
-[ver.2020 03 27]
+
---
+++ 上海实时公交查询Plus.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import requests
+import urllib
+from bs4 import BeautifulSoup
+def MD5(x):
+    import hashlib
+    
+    m = hashlib.md5()
+    b = x.encode(encoding='utf-8')
+    m.update(b)
+    str_md5 = m.hexdigest()
+    return str_md5
+try:
+    bnumber=input("请输入公交线路（例：71路，北安线）：")
+    bn=MD5(bnumber)
+    stoplist=[]        
+    
+    url1="https://shanghaicity.openservice.kankanews.com/public/bus/mes/sid/"+bn
+    r1 = requests.get(url1)
+    soup = BeautifulSoup(r1.text, 'html.parser')
+    stop_list = soup.find_all('span', class_='name')
+    for item in stop_list:       
+        a=item.text
+        stoplist.append(a)
+   
+    dire=input("请输入上下行(0："+stoplist[-1]+" 方向； 1:"+stoplist[0]+" 方向)：")
+    if dire=="1":
+        stoplist=[]
+        url2="https://shanghaicity.openservice.kankanews.com/public/bus/mes/sid/"+bn+"?stoptype=1"
+        r1 = requests.get(url2)
+        soup = BeautifulSoup(r1.text, 'html.parser')
+        stop_list = soup.find_all('span', class_='name')
+        for item in stop_list:       
+            a=item.text
+            stoplist.append(a)
+ 
+    j=1
+    present=[]
+    detail={}
+    for i in stoplist:          
+        stopid=str(j)
+        postdata={'stoptype':dire,'stopid':stopid,'sid':bn }
+        r=requests.post('https://shanghaicity.openservice.kankanews.com/public/bus/Getstop',data=postdata)
+        result=r.text
+        id=int(stopid)-1
+       # print("\n",bnumber," ",stoplist[id]," 车站 实时公交信息\n")
+        
+        if result=='{"error":"-2"}':
+            #print("暂未发车\n")
+            pass
+        else:
+            rs=result.strip('[\'{').strip(']}\'').split(",")
+            busnumber=((rs[1].strip('\'').strip('\"').split(":"))[1].strip('\"')).encode('utf-8').decode("unicode-escape")
+            stopdis=((rs[2].strip('\'').strip('\"').split(":"))[1].strip('\"'))
+            distance=((rs[3].strip('\'').strip('\"').split(":"))[1].strip('\"'))
+            time=((rs[4].strip('\'').strip('\"').split(":"))[1].strip('\"'))                    
+            #print(busnumber,"\n","距离本站：",stopdis,"站，",distance,"m\n","预计",time,"s后到达本站\n")
+            if int(stopdis)==1:
+                present.append(id)
+                detail[id]=busnumber
+        j=j+1
+        
+    try:
+        print("\n",bnumber," 开往：",stoplist[-1],"方向  \n 沿途站点：\n")
+        i=1
+        for it in stoplist:
+            if i in present:
+                number=detail[i]
+                print(str(i),".",stoplist[i-1],"  🚌↓ ",number)
+            else:
+                print(str(i),".",stoplist[i-1])
+            i=i+1
+    except:
+        pass
+except:
+    exit()
